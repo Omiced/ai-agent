8a667f875af19a9699f0d7d7f50daf2fffb16171
Josue Cano <jdevelopcano@gmail.com>
2026-01-29 11:45:06 -0600

    added cosas xd

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt 
+from call_function import available_functions
 
 def main():
   load_dotenv()
@@ -21,7 +23,13 @@ def main():
 
   messages=[types.Content(role="user",parts=[types.Part(text=args.user_prompt)])]
 
-  response = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
+  response = client.models.generate_content(model="gemini-2.5-flash",
+                                            contents=messages,
+                                            config=types.GenerateContentConfig(
+                                              tools=[available_functions],
+                                              system_instruction=system_prompt, 
+                                              temperature=0)
+                                            )
   if response.usage_metadata == None:
     raise RuntimeError("Failed api request")
 
@@ -29,7 +37,11 @@ def main():
     print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}") 
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}") 
-  print(response.text)
+  if response.function_calls:
+    for function_call in response.function_calls:
+      print(f"Calling function: {function_call.name}({function_call.args})")
+  else:
+    print(response.text)
 
   
 
